bfd7c5ccfd632a2fa938086aefb6a47b898ed7cb
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2018-10-25 12:41:12 +0200

    Ejercicios DWES

--- DWES/Session y cookies/Entrega (Daniel Barragues)/Ejercicio 2/index.php
+++ DWES/Session y cookies/Entrega (Daniel Barragues)/Ejercicio 2/index.php
@@ -2,14 +2,38 @@
 <?php
     session_start();
     if (!isset($_SESSION["productos"])) {
-        $miBand3 = ["id"=>001, "nombre"=>"Mi Band 3","descripcion"=>"blah","precio"=>29];
-        $amazfitPace = ["id"=>002, "nombre"=>"Amazfit Pace","descripcion"=>"blah","precio"=>110];
-        $amazfitStratos = ["id"=>003, "nombre"=>"Amazfit Stratos","descripcion"=>"blah","precio"=>140];
-        $bip = ["id"=>004, "nombre"=>"Bip","descripcion"=>"blah","precio"=>80];
-        $_SESSION["productos"] = ["miBand3"=>$miBand3,"amazfitPace"=>$amazfitPace,"amazfitStratos"=>$amazfitStratos,"bip"=>$bip];
+        $miBand3 = [
+            "id"=>001,
+            "nombre"=>"Mi Band 3",
+            "descripcion"=>"Ya está aquí la nueva pulsa de Xiaomi. Nuevo hardware, más batería y más precisa.",
+            "precio"=>29];
+        $amazfitPace = [
+            "id"=>002,
+            "nombre"=>"Amazfit Pace",
+            "descripcion"=>"Para corredores natos. El mejor SmartWatch de la mejor compañía.",
+            "precio"=>110];
+        $amazfitStratos = [
+            "id"=>003,
+            "nombre"=>"Amazfit Stratos",
+            "descripcion"=>"Si pides un diseño más premium, más precisión y estimación de VO2 este SmartWatch es para tí.",
+            "precio"=>140];
+        $bip = [
+            "id"=>004,
+            "nombre"=>"Bip",
+            "descripcion"=>"¿Necesitas una batería que dura más de 1 mes, GPS y un monitor de actividad? Esta pulsera es para tí.",
+            "precio"=>80];
+        $_SESSION["productos"] = [
+            "miBand3"=>$miBand3,
+            "amazfitPace"=>$amazfitPace,
+            "amazfitStratos"=>$amazfitStratos,
+            "bip"=>$bip];
     }
     if (!isset($_SESSION["carritoCompraSession"])) {
-        $_SESSION["carritoCompraSession"] = ["miBand3"=>0,"amazfitPace"=>0,"amazfitStratos"=>0,"bip"=>0];
+        $_SESSION["carritoCompraSession"] = [
+            "miBand3"=>0,
+            "amazfitPace"=>0,
+            "amazfitStratos"=>0,
+            "bip"=>0];
     }
 ?>
 <!DOCTYPE html>
@@ -24,8 +48,9 @@
 
     <body>
         <h1>Amazonias Shopping</h1>
-        <img class="carrito" onclick="mostrarPanel();" src="media/carrito.png">
-        <div id="panelCarrito">
+        <img class="carrito" onclick="panelCarrito(true);" src="media/carrito.png">
+        <div class="panelCarrito" id="panelCarrito">
+            <p id="relleno">No hay nada en tu carrito</p>
             <?php
                 if (isset($_POST["submit"])) {
                     $carritoCompra = $_SESSION["carritoCompraSession"];
@@ -39,7 +64,7 @@
                     }
                 }
             ?>
-            <button class="cerrarPanel" onclick="ocultarPanel();">Cerrar carro</button>
+            <button class="btnCerrarPanel" onclick="panelCarrito();">Cerrar carro</button>
         </div>
         <div class="contenedor">
             <?php
@@ -51,13 +76,18 @@
                     echo "<img class='fotoProducto' src='media/", $key, ".jpg'><br/>";
                     echo "<p>", $value["descripcion"], "</p>";
                     echo "<form action='index.php' method='POST'>
-                        <button class='botonCompra' name='submit' type='submit' value='", $key, "'>Añadir al carrito</button>
-                    </form>";
+                            <button class='botonCompra' onclick='quitarRelleno();' name='submit' type='submit' value='", $key, "'>Añadir al carrito</button>
+                        </form>";
                     echo "</div>";
                     $i++;
                 }
             ?>
         </div>
     </body>
+    <script>
+        window.onload = function mantenerCarrito() {
+            panelCarrito();
+        }
+    </script>
 
 </html>
--- DWES/Session y cookies/Entrega (Daniel Barragues)/Ejercicio 2/js.js
+++ DWES/Session y cookies/Entrega (Daniel Barragues)/Ejercicio 2/js.js
@@ -1,7 +1,23 @@
-function mostrarPanel() {
-    document.getElementById("panelCarrito").style.display = "block";
+function panelCarrito(abrir) {
+    if (localStorage.getItem("carritoAbierto") == "true" || abrir == true) {
+        document.getElementById("panelCarrito").classList.add("mostrarPanel");
+        localStorage.setItem("carritoAbierto", true);
+    } else {
+        document.getElementById("panelCarrito").classList.remove("mostrarPanel");
+        localStorage.setItem("carritoAbierto", false);
+    }
 }
 
-function ocultarPanel() {
-    document.getElementById("panelCarrito").style.display = "none";
+function quitarRelleno() {
+    document.getElementById("relleno").style.display = "none";
+    localStorage.setItem("rellenoQuitado", true);
+}
+
+function ponerRelleno() {
+    document.getElementById("relleno").style.display = "none";
+    localStorage.setItem("rellenoQuitado", true);
+}
+
+window.close = function borrarLocal() {
+    localStorage.clear();
 }
